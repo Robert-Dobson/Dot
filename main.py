5c60fa3e9d1812953c152677133183ebc6739419
Robert Dobson <43008203+Robert-Dobson@users.noreply.github.com>
2023-08-16 18:17:08 +0000

    Add a random number generator

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import asyncio
 import discord
 from discord.ext import commands
 from dotenv import load_dotenv
+import random as rand
 import logging
 import os
 
@@ -17,6 +18,22 @@ for filename in os.listdir("cogs"):
 @bot.command()
 async def test(ctx):
     await ctx.send("Testing 123")
+
+
+@bot.command()
+async def random(ctx, *args):
+    if len(args) == 0:
+        await ctx.send("You must provide an arugment (a number)")
+        return
+    elif len(args) > 1:
+        await ctx.send("Too many arguments")
+        return
+
+    if not args[0].isnumeric():
+        await ctx.send("Must provide a number")
+        return
+
+    await ctx.send(f"Random number is: {rand.randint(1, int(args[0]))}")
 
 
 # Run Bot
